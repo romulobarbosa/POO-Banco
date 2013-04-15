68cc2687e31e317c17f42f8b23218bceabb14155
Rômulo Barbosa <barbosaromulo@gmail.com>
2013-04-15 17:12:55 -0300

    Adiciona os arquivos feitos aula

---
+++ src/Agencia.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+/**
+ * Agencia
+ * 
+ * @author gabriel
+ */
+public class Agencia { 
+	
+/**
+ * Número máximo de contas
+ */
+	private byte MAX_CONTAS = 20;
+	
+/**
+ * Lista das contas da agência
+ */
+	private ArrayList<Conta> contas;
+	
+/**
+ * Contrutor da classe agência
+ */
+	public Agencia() {}
+	
+/**
+ * Cria conta
+ * 
+ * @param numero
+ * @param proprietario
+ * @param saldo
+ * @return
+ */
+	public static String criarConta(int numero, String proprietario, float saldo) {
+		if (saldo < 0 ) {
+			return "Vai se ferra valor negativo";
+		}
+		
+		Conta conta1 = new Conta(001, "Romulo Barbosa", (float) -100.00);
+		return "";
+	}	
+}
---
+++ src/Conta.java
@@ -0,0 +1,110 @@
+/**
+ * Conta
+ * 
+ * @author gabriel
+ */
+public class Conta {
+	private byte OPERACAO_OK = 0;
+	private byte VALOR_NEGATIVO = 1;
+	private byte SALDO_INSUFICIENTE = 2;
+	
+/**
+ * Nœmero da conta
+ */
+	private int numero;
+	
+/**
+ * Propriet‡rio da conta
+ */
+	private String proprietario;
+	
+/**
+ * Saldo da conta
+ */
+	private float saldo;
+	
+/**
+ * Contrutor da classe conta
+ * 
+ * @param numero
+ * @param proprietario
+ * @param saldo
+ */
+	public Conta(int numero, String proprietario, float saldo) {
+		this.numero = numero;
+		this.proprietario = proprietario;
+		this.saldo = saldo;
+	}
+	
+/**
+ * Retorna o nœmero da conta
+ * 
+ * @return int numero
+ */
+	public int getNumero() {
+		return this.numero;
+	}
+	
+/**
+ * Retorna o nome do propriet‡rio da conta
+ * 
+ * @return String propriet‡rio
+ */
+	public String getProprietario() {
+		return this.proprietario;
+	}
+	
+/**
+ * Retorna o saldo atual da conta
+ * 
+ * @return float saldo
+ */
+	public float getSaldo() {
+		return this.saldo;
+	}
+
+/**
+ * Lista os dados
+ * 
+ * @return
+ */
+	public String listarDados() {
+		return "Nœmero: " + this.getNumero() + ", Propriet‡rio: " + this.getProprietario() + ", Saldo: " + this.getSaldo();
+	}
+
+/**
+ * Retira um valor da conta
+ * 
+ * @param valor
+ * @return
+ */
+	public byte sacar(float valor) {
+		if (valor < 0) {
+			return VALOR_NEGATIVO;
+		}
+		
+		if (this.getSaldo() < valor) {
+			return SALDO_INSUFICIENTE;
+		}
+		
+		this.saldo = this.saldo - valor;
+		
+		return OPERACAO_OK;
+	}
+	
+/**
+ * Deposita um valor na conta
+ * 
+ * @param valor
+ * @return
+ */
+	public byte depositar(float valor) {
+		if (valor < 0) {
+			return VALOR_NEGATIVO;
+		}
+		
+		this.saldo = this.saldo + valor;
+		
+		return OPERACAO_OK;
+	}
+}
