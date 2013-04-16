5121865f3b3ce23db51e57c4d44a60ee9fd56615
Rômulo Barbosa <barbosaromulo@gmail.com>
2013-04-16 20:48:53 -0300

    Mensagem de erro caso o usuario digite uma opção invalida do menu

--- src/principal.java
+++ src/principal.java
@@ -7,9 +7,9 @@ public class principal {
 		Agencia agencia = new Agencia();
 		
 		int menuNum = 1;
-		while(menuNum != 0){
+		while(menuNum != 6){
 		
-			System.out.println("========================================");
+			System.out.println("====================||=====================");
 	        System.out.println("Sistema Bancario - Operacoes Disponiveis\n");
 	        System.out.println("1 - Criar conta");
 	        System.out.println("2 - Cancelar conta");
@@ -18,6 +18,12 @@ public class principal {
 	        System.out.println("5 - Listar contas existentes");
 	        System.out.println("6 - Finalizar programa\n");
 	        System.out.print("Selecione uma operacao: ");
+	        
+	        menuNum = dados.nextInt();
+	        
+	        if(menuNum == 0 || menuNum >= 7){
+	        	System.out.println("Opcao invalida");
+	        }
 		}
 
 	}
