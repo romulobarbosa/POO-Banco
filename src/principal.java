f8ce0c2f36fc77ab34fd8fd19a49331e50905a45
Rômulo Barbosa <barbosaromulo@gmail.com>
2013-04-16 20:18:36 -0300

    Adiciona os itens do menu

--- src/principal.java
+++ src/principal.java
@@ -4,6 +4,16 @@ public class principal {
 
 	public static void main(String[] args) {
 		Scanner dados = new Scanner(System.in);
+		
+		System.out.println("========================================");
+        System.out.println("Sistema Bancario - Operacoes Disponiveis\n");
+        System.out.println("1 - Criar conta");
+        System.out.println("2 - Cancelar conta");
+        System.out.println("3 - Sacar");
+        System.out.println("4 - Depositar");
+        System.out.println("5 - Listar contas existentes");
+        System.out.println("6 - Finalizar programa\n");
+        System.out.print("Selecione uma operacao: ");
 
 	}
 
