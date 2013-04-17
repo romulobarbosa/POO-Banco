4603ace502c6ca21540e99edf4bfc10469ae5b32
Rômulo Barbosa <barbosaromulo@gmail.com>
2013-04-16 21:51:15 -0300

    Imprime linhas em branco para facilitar a leitura ao usuario

--- src/principal.java
+++ src/principal.java
@@ -20,6 +20,7 @@ public class principal {
 	        System.out.print("Selecione uma operacao: ");
 	        
 	        menuNum = dados.nextInt();
+	        System.out.println();
 	        
 	        if(menuNum == 0 || menuNum >= 7){
 	        	System.out.println("Opcao invalida");
@@ -38,7 +39,11 @@ public class principal {
 		                System.out.print("Informe o saldo: ");
 		                saldo = dados.nextFloat();
 		                
+		                System.out.println();
 		                System.out.println(agencia.criarConta(numero, proprietario, saldo));
+		                System.out.println();
+		                System.out.println();
+		                break;
 		                
 		        	case 2:
 		        		System.out.println("2 - Cancelar conta");
@@ -46,16 +51,38 @@ public class principal {
 		        		System.out.println("Informe o numero da conta a ser cancelada");
 		        		num = dados.nextInt();
 		        		
+		        		System.out.println();
 		        		System.out.println(agencia.cancelarConta(num));
+		        		System.out.println();
+		                System.out.println();
+		        		break;
 		        	
 		        	case 3:
 		        		System.out.println("3 - Realizar um saque");
 		        		
+		        		System.out.println();
+		        		System.out.println();
+		        		System.out.println();
+		                System.out.println();
+		        		break;
+		        		
 		        	case 4:
 		        		System.out.println("4 - Realizar um deposito");
 		        		
+		        		System.out.println();
+		        		System.out.println();
+		        		System.out.println();
+		                System.out.println();
+		        		break;
+		        		
 		        	case 5:
 		        		System.out.println("5 - Listar contas existentes");
+		        		
+		        		System.out.println();
+		        		System.out.println();
+		        		System.out.println();
+		                System.out.println();
+		        		break;
 	        	}
 	        }
 	        
