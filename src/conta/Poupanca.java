600f3abab4ca07006f5ff16386afd715ad8c7fec
Gabriel Izaias <gabriel.izaias@gmail.com>
2013-06-26 23:22:30 -0300

    Adiciona comentário do método contrutor da classe `Poupanca`

--- src/conta/Poupanca.java
+++ src/conta/Poupanca.java
@@ -5,6 +5,15 @@ import execao.ExcecaoValorNegativo;
 
 public class Poupanca extends Conta {
 
+/**
+ * Cria uma nova inst‰ncia e inicializa atributos.
+ * 
+ * @param numero
+ * @param proprietario
+ * @param saldo
+ * @throws ExcecaoNumeroInvalido
+ * @throws ExcecaoValorNegativo
+ */
 	public Poupanca(int numero, String proprietario, float saldo) throws ExcecaoNumeroInvalido, ExcecaoValorNegativo {
 		super(numero, proprietario, saldo);
 	}
