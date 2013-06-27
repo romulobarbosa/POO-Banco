654617697edacd53e17e2672a85f127a5c79dbfd
Gabriel Izaias <gabriel.izaias@gmail.com>
2013-06-26 22:44:12 -0300

    Adiciona método construtor da classe `Corrente`

--- src/conta/Corrente.java
+++ src/conta/Corrente.java
@@ -1,9 +1,31 @@
 package conta;
 
-public class Corrente {
+import execao.ExcecaoNumeroInvalido;
+import execao.ExcecaoValorNegativo;
 
-	public static void main(String[] args) {
+public class Corrente extends Conta {
+	
+/**
+ * A tarifa de manutenção da conta.
+ */
+	private float tarifaManutencao;
 
+/**
+ * Cria uma nova instância e inicializa atributos.
+ * 
+ * @param numero
+ * @param proprietario
+ * @param saldo
+ * @throws ExcecaoNumeroInvalido
+ * @throws ExcecaoValorNegativo
+ */
+	public Corrente(int numero, String proprietario, float saldo) throws ExcecaoNumeroInvalido, ExcecaoValorNegativo {
+		super(numero, proprietario, saldo);
+	}
+
+	@Override
+	public String getTipoConta() {
+		return null;
 	}
 
 }
