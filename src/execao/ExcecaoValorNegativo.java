2bc7d4d1e1c14d866359a2a5c10e39c48ff8aafe
Rômulo Barbosa <barbosaromulo@gmail.com>
2013-06-29 20:30:03 -0300

    Imprime a mensagem de erro do metodo ExcecaoValorNegativo() da Classe ExcecaoValorNegativo

--- src/execao/ExcecaoValorNegativo.java
+++ src/execao/ExcecaoValorNegativo.java
@@ -1,9 +1,16 @@
 package execao;
+import geral.Utilitarios;
 
 public class ExcecaoValorNegativo extends RuntimeException {
+	private static Utilitarios u = new Utilitarios();
 
 	public ExcecaoValorNegativo(String message) {
-		super(message);
+		// super(message);
+		u.pl(u.t(15, "*"));
+		u.pl("Ocorreu um erro na operação. Tente novamente.");
+		u.pl("Detalhe do erro: ExcecaoValorNegativo: " + message);
+		u.pl(u.t(15, "*"));
+		
 	}
 
 }
