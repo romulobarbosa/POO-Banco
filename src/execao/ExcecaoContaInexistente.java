ce45a39ac6e45d30d2c55422ec60d61e1cdaf586
Rômulo Barbosa <barbosaromulo@gmail.com>
2013-06-29 20:58:52 -0300

    Generaliza o metodo ExcecaoContaExistente() da Classe ExcecaoContaExistente

--- src/execao/ExcecaoContaInexistente.java
+++ src/execao/ExcecaoContaInexistente.java
@@ -7,7 +7,7 @@ public class ExcecaoContaInexistente extends RuntimeException {
 	public ExcecaoContaInexistente(String message) {
 		// super(message);
 		u.pl(u.t(15, "*"));
-		u.pl("Ocorreu um erro ao realizar a consulta. Tente novamente.");
+		u.pl("Ocorreu um erro ao realizar essa operação. Tente novamente.");
 		u.pl("Detalhe do erro: ExcecaoContaExistente: " + message);
 		u.pl(u.t(15, "*"));
 	}
