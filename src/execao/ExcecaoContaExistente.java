b19f7ee1d150ebd3b92842cfd77378029bde0dfd
Rômulo Barbosa <barbosaromulo@gmail.com>
2013-06-29 19:17:12 -0300

    Imprime a mensagem de erro do metodo ExcecaoContaExistente() da Classe ExcecaoContaExistente

--- src/execao/ExcecaoContaExistente.java
+++ src/execao/ExcecaoContaExistente.java
@@ -1,9 +1,15 @@
 package execao;
+import geral.Utilitarios;
 
 public class ExcecaoContaExistente extends RuntimeException {
+	private static Utilitarios u = new Utilitarios();
 
 	public ExcecaoContaExistente(String message) {
-		super(message);
+		// super(message);
+		u.pl(u.t(15, "*"));
+		u.pl("Ocorreu um erro na validacao da opcao. Tente novamente.");
+		u.p("Detalhe do erro: ExcecaoOpcaoInvalida: " + message);
+		u.pl(u.t(15, "*"));
 	}
 
 }
