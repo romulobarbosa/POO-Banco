05cec4782849d6c6978082fdbcfa4ff9efa8bd33
Rômulo Barbosa <barbosaromulo@gmail.com>
2013-06-30 16:07:37 -0300

    Corrige erro nas classes de exceção

--- src/execao/ExcecaoContaInexistente.java
+++ src/execao/ExcecaoContaInexistente.java
@@ -1,14 +1,12 @@
 package execao;
 import geral.Utilitarios;
 
+@SuppressWarnings("serial")
 public class ExcecaoContaInexistente extends RuntimeException {
 	private static Utilitarios u = new Utilitarios();
 
 	public ExcecaoContaInexistente(String message) {
-		// super(message);
-		u.pl(u.t(15, "*"));
-		u.pl("Ocorreu um erro ao realizar essa operação. Tente novamente.");
-		u.pl("Detalhe do erro: ExcecaoContaExistente: " + message);
-		u.pl(u.t(15, "*"));
+		super(message);
+		// u.pl("Detalhe do erro: ExcecaoContaExistente: " + message);
 	}
 }
--- src/execao/ExcecaoSaldoInsuficiente.java
+++ src/execao/ExcecaoSaldoInsuficiente.java
@@ -1,15 +1,13 @@
 package execao;
 import geral.Utilitarios;
 
+@SuppressWarnings("serial")
 public class ExcecaoSaldoInsuficiente extends RuntimeException {
 	private static Utilitarios u = new Utilitarios();
 
 	public ExcecaoSaldoInsuficiente(String message) {
-		// super(message);
-		u.pl(u.t(15, "*"));
-		u.pl("Ocorreu um erro ao realizar a consulta. Tente novamente.");
-		u.p("Detalhe do erro: ExcecaoSaldoInsuficiente: " + message);
-		u.pl(u.t(15, "*"));
+		super(message);
+		// u.p("Detalhe do erro: ExcecaoSaldoInsuficiente: " + message);
 	}
 
 }
